f2cb6ae3d4ac5d9d0e647ca3421cc3bc0146d11b
nahid18 <nahidpatwary50@gmail.com>
2024-05-25 17:30:41 +0600

    feat: pkgdown init

--- R/get_study.R
+++ R/get_study.R
@@ -1,19 +1,20 @@
-#' Get pyTCR metrics of a study
-#' Retrieve metrics based on project ID.
+#' Get pyTCR data of a project
 #'
-#' @param id A character string specifying the ID of the data for which metrics are to be retrieved.
-#' @return A data frame containing the requested metrics.
+#' @param id Project ID.
+#' @return Data frame with the project data.
 #' @export
 #'
 #' @examples
-#' \dontrun{
-#' # Retrieve project with ID "PRJNA473147"
+#' \dontrun{ 
+#' # Get data with ID "PRJNA473147"
 #' project <- reTCR::get_study(id="PRJNA473147")
 #' }
-#'
-#' @importFrom utils read.csv
+#' @import utils
+#' @import methods
 
 get_study <- function(id) {
   stopifnot(is.character(id) && nchar(id) > 0)
-  return(invisible(read.csv(system.file("extdata", "PRJNA473147_mixcr_metadata_file.csv", package = "reTCR"))))
+  return(invisible(read.csv(
+    system.file("extdata", "PRJNA473147_mixcr_metadata_file.csv", package = "reTCR")
+  )))
 }
