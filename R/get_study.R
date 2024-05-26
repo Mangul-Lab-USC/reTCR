93d8506836f305b54ce43c70f68154a43f3f51ec
nahid18 <nahidpatwary50@gmail.com>
2024-05-26 11:26:07 +0600

    feat: finalize initial class setup

--- R/get_study.R
+++ R/get_study.R
@@ -1,20 +1,36 @@
 #' Get pyTCR data of a project
 #'
-#' @param id Project ID.
-#' @return Data frame with the project data.
+#' Retrieve the pyTCR data of a project by its ID.
+#'
+#' @param id Character. Project ID.
+#' @return An object of class `reTCRProj`.
 #' @export
 #'
 #' @examples
-#' \dontrun{ 
-#' # Get data with ID "PRJNA473147"
-#' project <- reTCR::get_study(id="PRJNA473147")
+#' \dontrun{
+#' proj <- reTCR::get_study(id = "PRJNA473147")
 #' }
-#' @import utils
-#' @import methods
+
+#' @importFrom methods new
+#' @importFrom utils read.csv
 
 get_study <- function(id) {
   stopifnot(is.character(id) && nchar(id) > 0)
-  return(invisible(read.csv(
-    system.file("extdata", "PRJNA473147_mixcr_metadata_file.csv", package = "reTCR")
-  )))
+
+  data <- read.csv(
+    system.file(
+      "extdata",
+      paste0(id, "_mixcr_metadata_file.csv"),
+      package = "reTCR"
+    )
+  )
+
+  motif <- new("Motif", aa_count = 10, nucl_count = 20)
+  diversity <- new("Diversity", shannon_index = 1.5, gini_index = 0.3)
+
+  return(new("reTCRProj",
+    data = data,
+    motif = motif,
+    diversity = diversity
+  ))
 }
---
+++ R/retcr_proj_class.R
@@ -0,0 +1,44 @@
+#' Class "Motif"
+#'
+#' A class to represent motif data.
+#'
+#' @slot aa_count A numeric value representing the amino acid count.
+#' @slot nucl_count A numeric value representing the nucleotide count.
+#' @exportClass Motif
+setClass("Motif",
+  slots = c(
+    aa_count = "numeric",
+    nucl_count = "numeric"
+  )
+)
+
+#' Class "Diversity"
+#'
+#' A class to represent diversity data.
+#'
+#' @slot shannon_index A numeric value representing the Shannon index.
+#' @slot gini_index A numeric value representing the Gini index.
+#' @exportClass Diversity
+setClass("Diversity",
+  slots = c(
+    shannon_index = "numeric",
+    gini_index = "numeric"
+  )
+)
+
+#' Class "reTCRProj"
+#'
+#' A class to represent a reTCR project
+#'
+#' @slot data contains the main data for the project.
+#' @slot motif contains motif information.
+#' @slot diversity contains diversity information.
+#' @exportClass reTCRProj
+setClass("reTCRProj",
+  slots = c(
+    data = "data.frame",
+    motif = "Motif",
+    diversity = "Diversity"
+  )
+)
+
