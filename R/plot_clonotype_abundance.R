3158a7ec9870215e16a5a4eda984d2c212f6583d
nahid18 <nahidpatwary50@gmail.com>
2024-06-17 03:28:53 +0600

    chore: dynamic color

--- R/plot_clonotype_abundance.R
+++ R/plot_clonotype_abundance.R
@@ -15,7 +15,7 @@
 #' }
 plot_clonotype_abundance <- function(data) {
   label_order <- unique(data$clonotype_group)
-  colors <- c("#E41A1C", "#377EB8", "#4DAF4A", "#984EA3", "#FF7F00")
+  colors <- RColorBrewer::brewer.pal(n = length(label_order), name = "Set1")
 
   ggplot2::ggplot(
     data,
